5d707c001dedf53d2c6200a07481444188b61952
U-lg-PC\lg <lg@lg-PC.(none)>
2012-11-29 23:23:24 +0900

    world_clock.h

---
+++ p11-1/world_clock.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class WorldClock {
+  public:
+    WorldClock() : time_difference_(0) {}
+    WorldClock(int hour, int minute, int second) : hour_(hour), minute_(minute), second_(second) {}
+    void Tick(int seconds = 1);
+    bool SetTime(int hour, int minute, int second);
+    static void AddTimezoneInfo(const string& city, int diff);
+    bool SetTimezone(const string& timezone);
+    int hour() const { return hour_; }
+    int minute() const { return minute_; }
+    int second() const { return second_; }
+    int time_difference() const { return time_difference_; }
+  private:
+    int hour_;
+    int minute_;
+    int second_; 
+    int time_difference_;
+    static map<string, int> timezone_;
+};
+struct InvalidTimeException {
+    string input_time;
+    InvalidTimeException(const string& str) : input_time(str) {}
+};
+ostream& operator<<(ostream& os, const WorldClock& c);
+istream& operator>>(istream& is, WorldClock& c);
