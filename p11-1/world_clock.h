8f654d97fd305b28bd829b3742ea13ebbf05945d
U-lg-PC\lg <lg@lg-PC.(none)>
2012-12-04 20:06:34 +0900

    world_clock.h

--- p11-1/world_clock.h
+++ p11-1/world_clock.h
@@ -2,14 +2,15 @@
 #include <string>
 #include <map>
 
+
 class WorldClock {
   public:
     WorldClock() : time_difference_(0) {}
     WorldClock(int hour, int minute, int second) : hour_(hour), minute_(minute), second_(second) {}
     void Tick(int seconds = 1);
     bool SetTime(int hour, int minute, int second);
-    static void AddTimezoneInfo(const string& city, int diff);
-    bool SetTimezone(const string& timezone);
+    static void AddTimezoneInfo(const std::string& city, int diff);
+    bool SetTimezone(const std::string& timezone);
     int hour() const { return hour_; }
     int minute() const { return minute_; }
     int second() const { return second_; }
@@ -19,11 +20,11 @@ class WorldClock {
     int minute_;
     int second_; 
     int time_difference_;
-    static map<string, int> timezone_;
+    static std::map<std::string, int> timezone_;
 };
 struct InvalidTimeException {
-    string input_time;
-    InvalidTimeException(const string& str) : input_time(str) {}
+  std::string input_time;
+  InvalidTimeException(const std::string& str) : input_time(str) {}
 };
-ostream& operator<<(ostream& os, const WorldClock& c);
-istream& operator>>(istream& is, WorldClock& c);
+std::ostream& operator<<(std::ostream& os, const WorldClock& c);
+std::istream& operator>>(std::istream& is, WorldClock& c);
